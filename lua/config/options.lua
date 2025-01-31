8705fd32ef403eb7b4127ebf1496058a7babf5aa
David Garcia Tort <degete@outlook.com>
2025-01-31 17:38:43 +0900

    Move extras to lazyvim.json

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- In case you don't want to use `:LazyExtras`, then you need to set the option below.
+vim.g.lazyvim_picker = "snacks"
+
+-- Enable this option to avoid conflicts with Prettier.
+vim.g.lazyvim_prettier_needs_config = true
--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,12 +1,12 @@
 return {
   -- https://github.com/lewis6991/gitsigns.nvim
-  { "lewis6991/gitsigns.nvim" },
+  -- { "lewis6991/gitsigns.nvim" },
   -- https://github.com/tpope/vim-fugitive
-  { "tpope/vim-fugitive" },
+  -- { "tpope/vim-fugitive" },
   -- https://github.com/tpope/vim-rhubarb
-  { "tpope/vim-rhubarb" },
+  -- { "tpope/vim-rhubarb" },
   -- https://github.com/rhysd/git-messenger.vim
-  { "rhysd/git-messenger.vim" },
+  -- { "rhysd/git-messenger.vim" },
   -- https://github.com/sindrets/diffview.nvim
-  { "sindrets/diffview.nvim" },
+  -- { "sindrets/diffview.nvim" },
 }
